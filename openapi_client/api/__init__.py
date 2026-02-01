9b116cda382f725606a38a2ce9e5bf99376698f0
bardonadam <bardon.adam@gmail.com>
2026-02-01 11:50:01 +0100

    chore: remove legacy openapi_client package

--- openapi_client/api/__init__.py
+++
@@ -1,6 +0,0 @@
-# flake8: noqa
-
-# import apis into api package
-from openapi_client.api.live_activities_api import LiveActivitiesApi
-from openapi_client.api.push_notifications_api import PushNotificationsApi
-
